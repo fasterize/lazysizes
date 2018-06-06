656ef4e9c588409a6f17d44b2c71bfb17bb480de
stefounet <s@fasterize.com>
2018-06-06 23:21:49 +0200

    [#1010] Add Lazysizes as a new lazyloader for Fasterize

---
+++ index.js
@@ -0,0 +1,30 @@
+var fs    = require('fs'),
+    path  = require('path'),
+    merge = require('merge');
+
+var scriptsDir     = __dirname,
+		defaultOptions = {
+			lazyClass: 'lazyfrz',
+			autosizesClass: 'lazyautosizes',
+			srcAttr: 'data-frz-src',
+			srcsetAttr: 'data-frz-srcset',
+			sizesAttr: 'data-frz-sizes',
+			init: true,
+			loadHidden: false,
+		};
+
+
+function customizeScript (script, options) {
+  var opts = merge({}, defaultOptions, options);
+  return script + 'window.lazySizes.config={' + JSON.stringify(opts) + '};';
+}
+
+var script = fs.readFileSync(path.join(scriptsDir, 'lazysizes.min.js'), 'utf8');
+module.exports.script = function (options) {
+  return customizeScript(script, options);
+};
+
+var scriptDebug = fs.readFileSync(path.join(scriptsDir, 'lazysizes.js'), 'utf8');
+module.exports.scriptDebug = function (options) {
+  return customizeScript(scriptDebug, options);
+};
